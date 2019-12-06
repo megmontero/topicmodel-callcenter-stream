e17709c60df913b83cc981103a58f44a6cc48574
MANUEL ENRIQUE GOMEZ MONTERO <manuel.gomezmontero@telefonica.com>
2019-12-06 17:22:45 +0100

    JSON Null

--- src/main/java/com/telefonica/topicmodel/streamprocess/StreamPredicter.java
+++ src/main/java/com/telefonica/topicmodel/streamprocess/StreamPredicter.java
@@ -38,6 +38,7 @@ public class StreamPredicter {
         final KStream<String, Topic> topics = sequences.mapValues(
                 sequence -> {
                     ModelBajaFactura model = new ModelBajaFactura(sequence);
+                    logger.error("Sequence: " + sequence.sequence);
                     Topic topic = model.get_topic();
                     return topic;
                 }
