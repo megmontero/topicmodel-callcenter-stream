a4c48a0a27297958ac76d6970dbd77ee457c9cee
MANUEL ENRIQUE GOMEZ MONTERO <manuel.gomezmontero@telefonica.com>
2019-12-12 23:43:06 +0100

    New sequencer without start two streams

---
+++ src/main/java/com/telefonica/topicmodel/SequencerLauncherNew.java
@@ -0,0 +1,42 @@
+package com.telefonica.topicmodel;
+
+import com.telefonica.topicmodel.config.StreamConfig;
+import com.telefonica.topicmodel.streamprocess.StreamSequencer;
+import com.telefonica.topicmodel.streamprocess.StreamSequencerNew;
+import com.telefonica.topicmodel.view.VocabularyView;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class SequencerLauncherNew {
+
+    static Config config = ConfigFactory.load();
+    static final String inputTopic = config.getString("sequencer.inputTopic");
+    static final String outputTopic = config.getString("sequencer.outputTopic");
+    static final String vocabularyTopic = config.getString("sequencer.vocabularyTopic");
+    static final String applicationId = "topic.model.sequencer";
+    private final static int seguenceLenght = config.getInt("sequencer.sequenceLenght");
+
+    static final Logger logger = Logger.getLogger(SequencerLauncherNew.class);
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        final StreamConfig streamsConfiguration = new StreamConfig(applicationId);
+        final StreamsBuilder builder = new StreamsBuilder();
+        StreamSequencerNew.create_stream(builder,vocabularyTopic, inputTopic, outputTopic, seguenceLenght);
+
+
+        final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration.get_properties());
+
+        // Now run the processing topology via `start()` to begin processing its input data.
+        streams.start();
+
+        // Add shutdown hook to respond to SIGTERM and gracefully close the Streams application.
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    }
+
+
+}
---
+++ src/main/java/com/telefonica/topicmodel/streamprocess/StreamSequencerNew.java
@@ -0,0 +1,93 @@
+package com.telefonica.topicmodel.streamprocess;
+
+import com.telefonica.topicmodel.serdes.JsonPOJOSerdes;
+import com.telefonica.topicmodel.serdes.POJOClasses;
+import com.telefonica.topicmodel.serdes.POJOClasses.Sequence;
+import com.telefonica.topicmodel.serdes.POJOClasses.Token;
+import com.telefonica.topicmodel.view.VocabularyView;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamSequencerNew {
+    private static Serde<Token> tokenSerde;
+    private static Serde<Sequence> sequenceSerde;
+    final private static Logger logger = Logger.getLogger(StreamSequencer.class);
+    private final static String VOCABULARY_STORE= "vocabulary-store";
+    static public void create_stream(final StreamsBuilder builder, final String vocabularyTopic, final String inputTopic,
+                                     final String outputTopic, int seguenceLenght)
+    {
+        tokenSerde= JsonPOJOSerdes.getObjectSerde(Token.class);
+        sequenceSerde = JsonPOJOSerdes.getObjectSerde(Sequence.class);
+        KStream<String, Token> tokens = builder.stream(inputTopic, Consumed.with(Serdes.String(), tokenSerde));
+
+        final GlobalKTable<String, String> vocabulary = builder.globalTable (vocabularyTopic,
+                Materialized.  <String, String, KeyValueStore<Bytes, byte [] >> as
+                        (VOCABULARY_STORE) .withKeySerde(Serdes.String()).withValueSerde(Serdes.String()));
+
+
+
+        final ValueTransformerSupplier<Token, Sequence> valueTransformerSupplier =
+                () -> new ValueTransformer<Token, Sequence>() {
+                    private   ReadOnlyKeyValueStore<String, String> vocabularyView;
+                    @Override
+                    public void init(final ProcessorContext context) {
+                        vocabularyView = (ReadOnlyKeyValueStore) context.getStateStore(VOCABULARY_STORE);
+
+                    }
+
+                    @Override
+                    public Sequence transform(final Token token) {
+                        Sequence sequence = new Sequence();
+                        if (token.tokens != null) {
+                            List<Integer> sequenceList = Arrays.asList(token.tokens).stream().map(tkn -> {
+                                Integer val =0;
+                                try {
+                                    val= Integer.parseInt(vocabularyView.get(tkn));
+                                } catch (NumberFormatException e) {
+                                    logger.error("Token not found in vocabulary: " + token);
+                                }
+                                return val;
+                            }).collect(Collectors.toList());
+                            sequence.sequence = new Integer[seguenceLenght];
+                            Arrays.fill(sequence.sequence , 0);
+                            int start =  seguenceLenght - sequenceList.size();
+                            if (start <0)
+                                start=0;
+                            for (int i= start; i<seguenceLenght; i++)
+                            {
+                                sequence.sequence[i] = sequenceList.get(i - start);
+                            }
+                        }
+                        POJOClasses.copy_commons(sequence,token);
+
+                        return sequence;
+                    }
+
+                    @Override
+                    public void close() { }
+                };
+
+
+
+
+        final KStream <String, Sequence> sequences = tokens.transformValues(valueTransformerSupplier);
+
+
+
+
+        sequences.to(outputTopic, Produced.with(Serdes.String(), sequenceSerde));
+    }
+
+
+}
